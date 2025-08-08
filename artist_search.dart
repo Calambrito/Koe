4c6ae7a278c4247cab31470b245cedcf1ca93773
habibaalam <116030336+habibaalam@users.noreply.github.com>
2025-08-09 03:02:17 +0600

    Update artist_search.dart

--- artist_search.dart
+++ artist_search.dart
@@ -1,19 +1,4 @@
-/*import 'search_strategy.dart';
-import 'database_helper.dart';
-class ArtistSearchStrategy implements SongSearchStrategy {
-  final DatabaseHelper dbHelper = DatabaseHelper.getInstance();
 
-  @override
-  Future<List<Map<String, dynamic>>> search(String query) async {
-    final db = await dbHelper.database;
-    return await db.rawQuery('''
-      SELECT Songs.*
-      FROM Songs
-      JOIN Artist ON Songs.artist_id = Artist.artist_id
-      WHERE Artist.artist_name LIKE ?
-    ''', ['%$query%']);
-  }
-}*/
 import 'search_strategy.dart';
 import 'database_helper.dart';
 
@@ -31,3 +16,4 @@ class ArtistSearchStrategy implements SongSearchStrategy {
     ''', ['%$query%']);
   }
 }
+
